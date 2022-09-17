33ec7956e8a75ac33f75ae81bf1566ea09db186f
himajaabbina <83061371+himajaabbina@users.noreply.github.com>
2022-09-17 21:35:20 +0530

    num = int(input()) str_n=str(num) list_n=list(str_n) for i in range(len(str_n)):     if list_n[i]=="6":         list_n[i]="9"         break
    s=''.join(list_n)
    print(s)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -3,7 +3,8 @@ str_n=str(num)
 list_n=list(str_n)
 for i in range(len(str_n)):
     if list_n[i]=="6":
-       list_n[i]="9"
-       break
+        list_n[i]="9"
+        break
+    
 s=''.join(list_n)
 print(s)
